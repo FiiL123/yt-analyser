515003cda12ef9cc441b732f05ba6a0a65c53ff8
Filipko^^ <fiilgameslp@gmail.com>
2024-01-05 11:08:37 +0100

    add echarts and file upload

--- web/web/analyser/views.py
+++ web/web/analyser/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+import os
+
+from django.shortcuts import redirect, render
+
+from web import settings as web_settings
+
+from .forms import UploadFileForm
 
 
 def home(request):
-    return render(request, "home.html")
+    if request.method == "POST":
+        form = UploadFileForm(request.POST, request.FILES)
+        # print(request.FILES["file"])
+        if form.is_valid():
+            handle_uploaded_file(request.FILES["file"])
+            return redirect("home")
+    else:
+        form = UploadFileForm()
+    return render(request, "home.html", {"form": form})
+
+
+def handle_uploaded_file(uploaded_file):
+    # Define the upload path
+    upload_path = os.path.join(web_settings.MEDIA_ROOT, "uploads", uploaded_file.name)
+    os.makedirs(os.path.dirname(upload_path), exist_ok=True)
+    # Save the file to the server
+    with open(upload_path, "wb+") as destination:
+
+        for chunk in uploaded_file.chunks():
+            destination.write(chunk)
+
+    return upload_path
